4ab20a3b5c55a4ec7c368d14bf186a6512420029
kin_mugi <hmfxp35143@yahoo.co.jp>
2021-04-06 07:49:24 +0900

    herokuの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  #rootへのルーティング作成
+  #herokuでアプリケーションにアクセスしたとき、最初に開くページの設定
+  config.assets.compile = true
+  config.assets.initialize_on_precompile=false
 end
